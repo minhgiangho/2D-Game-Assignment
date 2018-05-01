50298b9e60c04565b8c47462e4db1c0e91f0f8ac
Latias94 <superfrankie621@gmail.com>
2018-05-02 00:04:31 +1000

    Create simple animator base on Kim's awesome sprites.

---
+++ Assets/Scripts/SimpleComtroller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleComtroller : MonoBehaviour
+{
+    public float speed = 14f;
+    private Animator animator;
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+//        if (Input.GetKeyDown(KeyCode.R))
+//        {
+//            _animator.SetBool("isFlash", true);
+//        }
+//        else if (Input.GetKeyDown(KeyCode.T))
+//        {
+//            _animator.SetBool("isAttack", true);
+//        }
+//        else if (Input.GetKeyDown(KeyCode.F))
+//        {
+//            _animator.SetBool("isRun", true);
+//        }
+
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Jump");
+
+        if (input.x > 0)
+        {
+            sr.flipX = false;
+        }
+        else if (input.x < 0)
+        {
+            sr.flipX = true;
+        }
+    }
+}
